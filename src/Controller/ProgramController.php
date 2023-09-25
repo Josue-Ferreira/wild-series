c918e018218eb7157f9e6fbd7d31382286cd5063
Ferreira Josué <ferreira.josu@gmail.com>
2023-09-25 19:43:53 +0200

    quest 1-5

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,30 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/program', name: 'program_')]
+class ProgramController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series'
+        ]);
+        // return new Response(
+        //     '<html><body>Wild Series Index</body></html>'
+        // );
+    }
+
+    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'id')]
+    public function identifier(int $id = 1): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+        ]);
+    } 
+}
